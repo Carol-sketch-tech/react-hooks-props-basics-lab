7fe3c6087db944af69dd3cdf5577555bc20388f4
Caroline <bancroftwanjiru10000@gmail.com>
2024-07-31 17:59:21 +0300

    not done

---
+++ src/components/links.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Links(props){
+    return(
+        <div>
+            <h3>Links
+        <a href="#">{props.github}</a>,
+        <a href="#">{props.linkedin}</a>
+        </h3>
+        </div>
+    )
+}
+
+export default Links;
